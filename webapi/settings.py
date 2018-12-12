ef2c3bb895d3217a07679be76e0f62281b4b2589
Francisco <franciscoigor@gmail.com>
2018-12-12 10:30:27 -0800

    static files config

--- webapi/settings.py
+++ webapi/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-#django_heroku.settings(locals())
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
+
+django_heroku.settings(locals())
