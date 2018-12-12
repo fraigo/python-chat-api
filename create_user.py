5c8ce4d88fc1a5199956dbf3394bf7df43f2d25c
Francisco <franciscoigor@gmail.com>
2018-12-12 09:21:17 -0800

    user create script

--- create_user.py
+++ create_user.py
@@ -1,5 +1,13 @@
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
-User.objects.create_superuser('admin', 'admin@myproject.com', 'Admin.123456')
+user_name = 'admin'
+user_pass = 'Admin.123456'
+user_email = 'admin@example.com'
 
+User = get_user_model()
+try:
+    admin = User.objects.get(username=user_name)
+    print("admin already created")
+except:
+    User.objects.create_superuser(user_name, user_email, user_pass)
+    print("admin created")
