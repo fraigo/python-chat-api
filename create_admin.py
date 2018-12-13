22239572edc8a37d2ea373675b2e4d89a10a465c
Francisco <franciscoigor@gmail.com>
2018-12-12 16:07:50 -0800

    Create admin password random

--- create_admin.py
+++ create_admin.py
@@ -1,13 +1,14 @@
+import random
 from django.contrib.auth import get_user_model
 
 user_name = 'admin'
-user_pass = 'Admin.123456'
+user_pass = "Admin.%s" % random.randint(10000000, 100000000)
 user_email = 'admin@example.com'
 
 User = get_user_model()
 try:
     admin = User.objects.get(username=user_name)
-    print("admin already created")
+    print("Admin already created")
 except:
     User.objects.create_superuser(user_name, user_email, user_pass)
-    print("admin created")
+    print("Admin created with pass [%s] " % user_pass)
